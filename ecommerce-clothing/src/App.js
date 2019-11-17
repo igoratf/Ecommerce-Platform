95278c0a0c4d7b34ede4902cfcbe8eec6cd643e4
igor <igor.atf@gmail.com>
2019-11-17 19:45:55 -0300

    add router to menu item

--- ecommerce-clothing/src/App.js
+++ ecommerce-clothing/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
