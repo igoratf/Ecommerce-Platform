6ae9deb55c896a44a0393cd9f4c8d5dfb384196f
igor <igor.atf@gmail.com>
2019-11-16 22:18:45 -0300

    add homepage draft

--- ecommerce-clothing/src/App.js
+++ ecommerce-clothing/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './HomePage';
 
 function App() {
   return (
     <div className="App">
+      <HomePage />
     </div>
   );
 }
