a474cfea71998adc3159ec0a439802699c262086
igor <igor.atf@gmail.com>
2019-11-23 17:42:09 -0300

    add sign up and store users in firestore

--- ecommerce-clothing/src/pages/sign-in-and-up/sign-in-and-up.component.jsx
+++ ecommerce-clothing/src/pages/sign-in-and-up/sign-in-and-up.component.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+
 import './sign-in-and-up.styles.scss';
+
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInAndUp = (props) => {
     console.log('PROPS ', props);
     return(
     <div className="sign-in-and-up">
         <SignIn />
+        <SignUp />
     </div>
 )}
 
