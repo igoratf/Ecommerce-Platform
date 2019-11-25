d03af654ea67ac1e4598c9b92a23cc9a426c1e43
igor <igor.atf@gmail.com>
2019-11-25 20:46:57 -0300

    redirect to home if user is signed

--- ecommerce-clothing/src/App.js
+++ ecommerce-clothing/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -49,22 +49,34 @@ class App extends Component {
   }
 
   render() {
+    const { currentUser } = this.props;
+
     return (
       <div className="App">
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" test="test" component={SignInAndUpPage} />
+          <Route exact 
+          path="/signin" 
+          render={() => 
+            currentUser ? 
+            (<Redirect to="/" />) 
+            : 
+            (<SignInAndUpPage />)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+})
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user))
 })
 
 
-export default connect(null, mapDispatchToProps) (App);
+export default connect(mapStateToProps, mapDispatchToProps) (App);
--- ecommerce-clothing/src/pages/sign-in-and-up/sign-in-and-up.component.jsx
+++ ecommerce-clothing/src/pages/sign-in-and-up/sign-in-and-up.component.jsx
@@ -5,8 +5,7 @@ import './sign-in-and-up.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up.component';
 
-const SignInAndUp = (props) => {
-    console.log('PROPS ', props);
+const SignInAndUp = () => {
     return(
     <div className="sign-in-and-up">
         <SignIn />
