e3eb8a20b6b97c49f6fc3448b2a6421192a6c8cd
igor <igor.atf@gmail.com>
2019-11-29 23:30:33 -0300

    add checkout page

--- ecommerce-clothing/src/redux/cart/cart.selectors.js
+++ ecommerce-clothing/src/redux/cart/cart.selectors.js
@@ -14,6 +14,11 @@ export const selectCartHidden = createSelector(
 
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
-    cartItems => 
+    (cartItems) => 
     cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+);
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    (cartItems) => cartItems.reduce((accumulatedPrice, cartItem) => accumulatedPrice + cartItem.quantity * cartItem.price, 0)
 )
