b0688a020ac8d46a52362043a96b05765ecdf542
igor <igor.atf@gmail.com>
2020-02-19 22:50:02 -0300

    improve styled components code and remove replaced components sass files

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -13,7 +13,7 @@ import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv } from './header.styles';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
     <HeaderContainer>
@@ -29,7 +29,7 @@ const Header = ({ currentUser, hidden }) => (
             </OptionLink>
             {
                 currentUser ?
-                    <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
+                    <OptionLink as='div' onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
                     : <OptionLink to="/signin">SIGN IN</OptionLink>
             }
             <CartIcon />
