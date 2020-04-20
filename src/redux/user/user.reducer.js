9b1e72ed8cf5c1316e4ccf03db39b184654a8bff
igor <igor.atf@gmail.com>
2020-04-20 18:15:58 -0300

    recreate sign out with sagas

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -9,11 +9,18 @@ const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case UserActionTypes.SIGN_IN_SUCCESS:
             return {
-                ...state, 
+                ...state,
                 currentUser: action.payload,
                 error: null
             }
+        case UserActionTypes.SIGN_OUT_SUCCESS:
+            return {
+                ...state,
+                currentUser: null,
+                error: null
+            }
         case UserActionTypes.SIGN_IN_FAILURE:
+        case UserActionTypes.SIGN_OUT_FAILURE:
             return {
                 ...state,
                 error: action.payload
